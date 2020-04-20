567c60ed9c226bc485a80059e965a99d5592aef7
Joaquin Matres <joaquin>
2020-04-20 10:22:44 -0700

    added waveguide tests

--- examples/example1.py
+++ examples/example1.py
@@ -1,4 +1,4 @@
-import modesolverpy.mode_solver as ms
+import modesolverpy.mode_solver_semi_vectorial as ms
 import modesolverpy.structure as st
 
 # All units are relative.  [um] were chosen in this case.
@@ -16,24 +16,24 @@ film_thickness = 0.5
 wavelength = 1.55
 angle = 75.0
 
-structure = st.RidgeWaveguide(
-    wavelength,
-    x_step,
-    y_step,
-    wg_height,
-    wg_width,
-    sub_height,
-    sub_width,
-    clad_height,
-    n_sub,
-    n_wg,
-    angle,
-    n_clad,
-    film_thickness,
+wg = st.waveguide(
+    x_step=0.02,
+    y_step=0.02,
+    wg_height=0.4,
+    wg_width=0.5,
+    sub_height=0.5,
+    sub_width=2.0,
+    clad_height=0.5,
+    n_sub=1.4,
+    n_wg=3.0,
+    n_clad=1.0,
+    film_thickness=0.5,
+    wavelength=1.55,
+    angle=75.0,
 )
 
-structure.write_to_file("example_structure_1.dat")
+st.write_material_index(wg)
 
 mode_solver = ms.ModeSolverSemiVectorial(2, semi_vectorial_method="Ey")
-mode_solver.solve(structure)
+mode_solver.solve(wg)
 mode_solver.write_modes_to_file("example_modes_1.dat")
