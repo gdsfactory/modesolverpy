62955fef627e78a9dd204403d1ad2c0fe542e630
joaquin <j>
2022-01-27 18:01:00 -0800

    fix ci/cd

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,10 @@
-from recommonmark.transform import AutoStructify
-
 project = "modes"
 version = "1.0.3"
 copyright = "2019, Jean-Luc Tambasco"
 author = "Jean-Luc Tambasco"
 
 master_doc = "index"
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 source_suffix = {
     ".rst": "restructuredtext",
@@ -16,17 +14,18 @@ source_suffix = {
 
 
 extensions = [
+    "matplotlib.sphinxext.plot_directive",
+    "myst_parser",
     "nbsphinx",
     "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
-    "matplotlib.sphinxext.plot_directive",
-    "sphinx_markdown_tables",
-    "sphinx.ext.doctest",
-    "recommonmark",
     "sphinx_autodoc_typehints",
+    "sphinx_click",
+    "sphinx_markdown_tables",
 ]
 
 autodoc_member_order = "bysource"
@@ -44,11 +43,16 @@ exclude_patterns = [
 
 napoleon_use_param = True
 
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
 
-def setup(app):
-    app.add_config_value(
-        "recommonmark_config",
-        {"auto_toc_tree_section": "Contents", "enable_eval_rst": True},
-        True,
-    )
-    app.add_transform(AutoStructify)
+language = "en"
+myst_html_meta = {
+    "description lang=en": "metadata description",
+    "description lang=fr": "description des métadonnées",
+    "keywords": "Sphinx, MyST",
+    "property=og:locale": "en_US",
+}
